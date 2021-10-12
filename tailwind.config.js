04e23bab086f9748b3f32155f50bcfd16d9e2d07
racmathafidz <rac.hafidz@gmail.com>
2021-10-12 18:23:58 +0700

    style(fontFamily): font family customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      serif: ['Merriweather', 'serif'],
+      sans: ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
