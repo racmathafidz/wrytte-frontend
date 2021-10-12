a5acb5870eadf0a7209f1daf0e04bcc6bb15846e
racmathafidz <rac.hafidz@gmail.com>
2021-10-12 18:46:21 +0700

    refactor(draftJS editor): custom editor's toolbar

---
+++ src/utils/toolbar.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+const toolbar = {
+  options: ['inline', 'blockType', 'list', 'textAlign', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
+  blockType: {
+    inDropdown: true,
+    options: ['Normal', 'H1', 'H2', 'H3', 'Blockquote', 'Code'],
+    className: undefined,
+    component: undefined,
+    dropdownClassName: undefined,
+  },
+};
+
+export default toolbar;
