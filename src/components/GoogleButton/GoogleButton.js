8d1eb5f3743b2a48352d5c960ba1b93e2b54a80b
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:09:11 +0700

    feat: add Google Button component

---
+++ src/components/GoogleButton/GoogleButton.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import Google from '../../assets/images/google.png';
+
+export default function GoogleButton(props) {
+  const { googleButtonHandler } = props;
+
+  return (
+    <button type="button" onClick={() => googleButtonHandler()} className="flex flex-row w-68 items-center bg-blue-500 rounded-lg py-1 pl-1 pr-4 shadow-lg transform transition duration-300 hover:shadow-2xl">
+      <div className="flex bg-white rounded-lg h-full mr-10 p-2">
+        <img src={Google} alt="Google" className="h-6 self-center" />
+      </div>
+      <p className="font-sans text-lg text-white py-2">Continue with Google</p>
+    </button>
+  );
+}
