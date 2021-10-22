4d62dbcd96a34c18db3262e61010c47957a3e0b4
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:15:41 +0700

    feat: add some component and fetch API

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-import Header from '../components/Header';
-import UserProfile from '../components/UserProfile';
-import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
+import UserProfile from '../components/UserProfile/UserProfile';
+import Footer from '../components/Footer/Footer';
+import LoadingPage from './LoadingPage';
 
 export default function ProfilePage() {
+  const { userName } = useParams();
+  const [profileData, setProfileData] = useState();
+
   useEffect(() => {
-    window.scroll(0, 0);
-  });
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/account/${userName}`)
+      .then((response) => setProfileData(response.data));
+  }, [userName]);
+
+  if (profileData) {
+    return (
+      <>
+        <Header />
+        <UserProfile profileData={profileData} />
+        <Footer />
+      </>
+    );
+  }
 
   return (
-    <>
-      <Header />
-      <UserProfile />
-      <Footer />
-    </>
+    <LoadingPage />
   );
 }
