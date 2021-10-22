1ef63f68bf3847e7c8822043026bce7e461b3621
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:15:58 +0700

    feat: add Loading Page

---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LoadingPage() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <p className="font-serif text-5xl text-gray-900 animate-bounce">W</p>
+    </div>
+  );
+}
