c4952d79c1e4496c5f815d2530d5a90c28caedf5
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:08:24 +0700

    feat: add Brand Icon component

---
+++ src/components/BrandIcon/BrandIcon.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function BrandIcon(props) {
+  const { Footer } = props;
+
+  return (
+    <NavLink to="/" className={`font-serif text-4xl ${Footer ? 'text-gray-100' : 'text-gray-900'}`}>
+      Wrytte
+    </NavLink>
+  );
+}
