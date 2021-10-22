2e5f297d9eeefb90735c81788f880b4165f70fed
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:17:19 +0700

    feat: add Date Formatter function

---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,10 @@
+/* eslint-disable max-len */
+export default function dateFormatter(rawDate) {
+  // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const dateArray = rawDate.split('T')[0].split('-').reverse();
+  const date = dateArray[0];
+  const month = months[dateArray[1] - 1];
+  const year = dateArray[2];
+  return `${date} ${month} ${year}`;
+}
