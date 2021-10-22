9c37b341720fb551067107febaff35a463977633
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:13:54 +0700

    feat: add some component and fetch API

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,17 +1,35 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import ArticlesList from '../components/ArticlesList';
-import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
+import Hero from '../components/Hero/Hero';
+import ArticlesList from '../components/ArticlesList/ArticlesList';
+import Footer from '../components/Footer/Footer';
+import LoadingPage from './LoadingPage';
 
 export default function LandingPage() {
+  const [articleData, setArticleData] = useState();
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/article`)
+      .then((response) => {
+        setArticleData(response.data);
+      });
+  }, []);
+
+  if (articleData) {
+    return (
+      <>
+        <Header />
+        <Hero />
+        <ArticlesList article={articleData} />
+        <Footer />
+      </>
+    );
+  }
+
   return (
-    <>
-      <Header />
-      <Hero />
-      <ArticlesList />
-      <Footer />
-    </>
+    <LoadingPage />
   );
 }
