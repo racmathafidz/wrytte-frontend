651c9b0927267b1d355e3eea51ed55ea19e7f5be
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 15:59:49 +0700

    fix(routing): put the route path that have params in bottom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-vars */
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Switch, Redirect,
+} from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import ArticlePage from './pages/ArticlePage';
@@ -12,11 +16,13 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/blog" component={ArticlePage} />
-      <Route exact path="/profile" component={ProfilePage} />
       <Route exact path="/signin" component={SignInPage} />
       <Route exact path="/signup" component={SignUpPage} />
       <Route exact path="/write" component={WritePostPage} />
+      {/* Put the path with params on the bottom. */}
+      <Route exact path="/:userName" component={ProfilePage} />
+      <Route exact path="/article/:articleTitle" component={ArticlePage} />
+      <Redirect from="/article" to="/" />
     </Switch>
   );
 }
