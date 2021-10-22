2a381660e9885a62a7d285ab4c62e3749841dfde
racmathafidz <rac.hafidz@gmail.com>
2021-10-22 16:14:36 +0700

    feat: add some component and fetch API

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,19 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-import Header from '../components/Header';
-import Article from '../components/Article';
-import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
+import Article from '../components/Article/Article';
+import Footer from '../components/Footer/Footer';
+import LoadingPage from './LoadingPage';
 
 export default function ArticlePage() {
+  const { articleTitle } = useParams();
+  const [articleData, setArticleData] = useState();
+  const [recomendation, setRecomendation] = useState();
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/article/${articleTitle}`)
+      .then((response) => {
+        setArticleData(response.data);
+      });
+  }, [articleTitle]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/article/recomendation`)
+      .then((response) => {
+        setRecomendation(response.data);
+      });
+  }, [articleTitle]);
+
   useEffect(() => {
     window.scroll(0, 0);
   });
 
+  if (articleData && recomendation) {
+    return (
+      <>
+        <Header />
+        <Article article={articleData} recomendation={recomendation} />
+        <Footer />
+      </>
+    );
+  }
+
   return (
-    <>
-      <Header />
-      <Article />
-      <Footer />
-    </>
+    <LoadingPage />
   );
 }
