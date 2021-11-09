2faf46b6f9711f4858a3925554d8c2c021e7b385
racmathafidz <rac.hafidz@gmail.com>
2021-11-09 12:39:00 +0700

    feat: add settings component and page

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,163 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+/* eslint-disable no-nested-ternary */
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { ActionCreators } from '../../redux/actions';
+import encrypt from '../../utils/encrypt';
+
+export default function Settings(props) {
+  const { profileData } = props;
+  const [selectedImage, setSelectedImage] = useState();
+  const [isUpdated, setIsUpdated] = useState(false);
+  const dispatch = useDispatch();
+  const { SignInAction } = bindActionCreators(ActionCreators, dispatch);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  console.log(errors);
+  const imageProfileChange = register('imageProfile');
+
+  function imageChange(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedImage(event.target.files[0]);
+    }
+  }
+
+  function saveHandler(data) {
+    if (data.imageProfile.length > 0) {
+      const uploadData = new FormData();
+      uploadData.append('file', selectedImage, 'file');
+      imageUpload(uploadData, data);
+    } else {
+      profileUpdate(data, undefined);
+    }
+  }
+
+  function imageUpload(imageToUpload, data) {
+    axios({
+      method: 'POST',
+      data: imageToUpload,
+      url: `${process.env.REACT_APP_BASE_URL}/api/account/image-upload`,
+    }).then(async (res) => {
+      profileUpdate(data, res.data.imageProfile);
+    });
+  }
+
+  function profileUpdate(data, changedImage) {
+    axios({
+      method: 'PUT',
+      data: {
+        email: data.email,
+        fullName: data.fullName,
+        userName: data.userName,
+        imageProfile: changedImage,
+      },
+      withCredentials: true,
+      url: `${process.env.REACT_APP_BASE_URL}/api/account/${profileData._id}`,
+    }).then((response) => {
+      const payload = {
+        id: response.data.id,
+        email: response.data.email,
+        fullName: response.data.fullName,
+        userName: response.data.userName,
+        imageProfile: response.data.imageProfile,
+        google: response.data.google,
+      };
+      SignInAction({
+        userData: encrypt(payload),
+      });
+      setIsUpdated(true);
+    });
+  }
+
+  return (
+    <div>
+      {
+        isUpdated && (
+          <div className="w-full py-4 bg-black text-center text-gray-100">Your profile has been updated</div>
+        )
+      }
+      <div className="container px-80 mt-12 pb-12 flex flex-col px-52 font-sans">
+        <p className="font-sans font-light text-gray-800 text-4xl mb-10">Account Settings</p>
+        <form onSubmit={handleSubmit(saveHandler)}>
+          <div className="flex flex-col">
+            <div className="grid grid-cols-2 gap-4 mb-6 items-center">
+              <p className="setting-label">Photo</p>
+              <label htmlFor="imageProfile">
+                <img
+                  src={selectedImage ? URL.createObjectURL(selectedImage) : `${profileData.imageProfile}`}
+                  alt="Author"
+                  className="rounded-full h-24 w-24 object-cover cursor-pointer"
+                />
+                <div className="absolute flex flex-row inline-flex items-center rounded-md ml-14 -mt-5 pl-2 pr-3 py-1 text-gray-100 bg-black cursor-pointer">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                  </svg>
+                  <p className="text-sm font-sans font-light"> Edit</p>
+                </div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="imageProfile"
+                  id="imageProfile"
+                  className="hidden h-10"
+                  {...imageProfileChange}
+                  onChange={(e) => {
+                    imageProfileChange.onChange(e);
+                    imageChange(e);
+                  }}
+                />
+              </label>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mb-6 items-center">
+              <p className="setting-label">Full Name</p>
+              <input
+                type="text"
+                name="fullName"
+                id="fullName"
+                data-testid="fullName"
+                className="setting-form"
+                defaultValue={profileData.fullName}
+                {...register('fullName', { required: true })}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4 mb-6 items-center">
+              <p className="setting-label">Username</p>
+              <input
+                type="text"
+                name="userName"
+                id="userName"
+                data-testid="userName"
+                className="setting-form"
+                defaultValue={profileData.userName}
+                {...register('userName', { required: true })}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4 mb-6 items-center">
+              <p className="setting-label">Email</p>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                data-testid="email"
+                className="setting-form"
+                defaultValue={profileData.email}
+                {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+              />
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-center mt-4">
+            <input type="submit" value="Save" data-testid="save" className="cursor-pointer py-2 px-6 bg-black text-gray-100 rounded-lg transform transition duration-300 hover:text-white hover:bg-gray-900" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/SettingsPage.js
@@ -0,0 +1,46 @@
+/* eslint-disable max-len */
+/* eslint-disable import/no-cycle */
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+import Header from '../components/Header/Header';
+import Settings from '../components/Settings/Settings';
+import Footer from '../components/Footer/Footer';
+import LoadingPage from './LoadingPage';
+import decrypt from '../utils/decrypt';
+
+export default function SettingsPage() {
+  const encryptedState = useSelector((state) => state);
+  const userDataState = encryptedState.UserData.userData ? decrypt(encryptedState.UserData.userData) : encryptedState.UserData;
+  const history = useHistory();
+  const [profileData, setProfileData] = useState();
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/api/account/${userDataState.userName}`)
+      .then((response) => setProfileData(response.data));
+  }, []);
+
+  useEffect(() => {
+    if (!encryptedState.UserData.userData) {
+      history.push('/signup');
+    }
+  }, [encryptedState]);
+
+  if (profileData) {
+    return (
+      <>
+        <Header />
+        <Settings profileData={profileData.profileData} />
+        <Footer />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <LoadingPage />
+    </>
+  );
+}
