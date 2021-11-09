a3f898386d1500a06c82b4508eece358da37af66
racmathafidz <rac.hafidz@gmail.com>
2021-11-09 12:34:41 +0700

    fix: fix article url on share buttton & img src

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -12,6 +12,7 @@ import ArticleMapping from '../ArticleMapping/ArticleMapping';
 
 export default function Article(props) {
   const { article, recomendation } = props;
+  const articleUrl = window.location.href;
 
   return (
     <div className="pb-8">
@@ -23,9 +24,9 @@ export default function Article(props) {
           >
             <LazyLoad>
               <img
-                src={`${process.env.REACT_APP_BASE_URL}/${article.authorData.imageProfile}`}
+                src={`${article.authorData.imageProfile}`}
                 alt="Author"
-                className="h-8 mr-2 rounded-full"
+                className="h-8 w-8 object-cover mr-2 rounded-full"
               />
             </LazyLoad>
           </NavLink>
@@ -41,7 +42,7 @@ export default function Article(props) {
           <p className="font-sans font-light text-lg">{dateFormatter(article.publishDate)}</p>
         </div>
         <LazyLoad>
-          <img src={`${process.env.REACT_APP_BASE_URL}/${article.imageCover}`} alt="Article Illustration" className="my-9" />
+          <img src={`${article.imageCover}`} alt="Article Illustration" className="my-9 mx-auto" />
         </LazyLoad>
         <p className="font-serif font-light text-lg text-justify leading-normal text-gray-900">{article.articleBody}</p>
         <div className="grid grid-cols-2 gap-4 my-9">
@@ -53,9 +54,9 @@ export default function Article(props) {
               >
                 <LazyLoad>
                   <img
-                    src={`${process.env.REACT_APP_BASE_URL}/${article.authorData.imageProfile}`}
+                    src={`${article.authorData.imageProfile}`}
                     alt="Author"
-                    className="h-16 mr-4 rounded-full"
+                    className="h-16 w-16 object-cover mr-4 rounded-full"
                   />
                 </LazyLoad>
               </NavLink>
@@ -71,16 +72,16 @@ export default function Article(props) {
           <div className="border border-gray-400 py-4 px-6 rounded-lg">
             <p className="text-lg text-gray-900">Share This Article</p>
             <div className="flex flex-row items-center mt-4">
-              <FacebookShareButton url="http://localhost:3000/blog" className="mr-2">
+              <FacebookShareButton url={`${articleUrl}`} className="mr-2">
                 <FacebookIcon size={48} round />
               </FacebookShareButton>
-              <TwitterShareButton url="http://localhost:3000/blog" className="mr-2">
+              <TwitterShareButton url={`${articleUrl}`} className="mr-2">
                 <TwitterIcon size={48} round />
               </TwitterShareButton>
-              <LinkedinShareButton url="http://localhost:3000/blog" className="mr-2">
+              <LinkedinShareButton url={`${articleUrl}`} className="mr-2">
                 <LinkedinIcon size={48} round />
               </LinkedinShareButton>
-              <WhatsappShareButton url="http://localhost:3000/blog" className="mr-2">
+              <WhatsappShareButton url={`${articleUrl}`} className="mr-2">
                 <WhatsappIcon size={48} round />
               </WhatsappShareButton>
             </div>
