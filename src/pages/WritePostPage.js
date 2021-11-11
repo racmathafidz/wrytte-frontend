f6114944c33cb1df474194dd140bdb1929c69173
racmathafidz <rac.hafidz@gmail.com>
2021-11-11 08:33:13 +0700

    feat(publish article): add publish article feature

--- src/pages/WritePostPage.js
+++ src/pages/WritePostPage.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable max-len */
 /* eslint-disable no-shadow */
 /* eslint-disable react/prop-types */
@@ -7,15 +9,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { NavLink, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import { convertToHTML } from 'draft-convert';
 import DOMPurify from 'dompurify';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 import Toolbar from '../utils/toolbar';
 import decrypt from '../utils/decrypt';
+import dashToSpace from '../utils/dashToSpace';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 export default function WritePostPage() {
@@ -24,6 +28,7 @@ export default function WritePostPage() {
   const history = useHistory();
   const [selectedImage, setSelectedImage] = useState();
   const [convertedContent, setConvertedContent] = useState(null);
+  const [isPublishing, setIsPublishing] = useState(false);
   const [editorState, setEditorState] = useState(
     () => EditorState.createEmpty(),
   );
@@ -58,9 +63,44 @@ export default function WritePostPage() {
   //   };
   // }
 
-  function postHandler(data) {
-    console.log(selectedImage);
-    console.log(data);
+  function urlTitle(string) {
+    return string.replace(/\s+/g, '-').toLowerCase();
+  }
+
+  function publishHandler(data) {
+    setIsPublishing(true);
+    const uploadImage = new FormData();
+    uploadImage.append('file', selectedImage, 'file');
+    imageUpload(uploadImage, data);
+  }
+
+  function imageUpload(uploadImage, data) {
+    axios({
+      method: 'POST',
+      data: uploadImage,
+      url: `${process.env.REACT_APP_BASE_URL}/picture-upload`,
+    }).then(async (res) => {
+      profileArticle(data, res.data.image);
+    });
+  }
+
+  function profileArticle(data, imageCover) {
+    axios({
+      method: 'POST',
+      data: {
+        imageCover,
+        articleTitle: data.title,
+        articleBody: convertedContent,
+        authorId: userDataState.id,
+        authorData: userDataState.id,
+      },
+      withCredentials: true,
+      url: `${process.env.REACT_APP_BASE_URL}/api/article/new-article`,
+    }).then((response) => {
+      if (response.data.articleTitle) {
+        history.push(`/article/${urlTitle(response.data.articleTitle)}`);
+      }
+    });
   }
 
   function goBack() {
@@ -75,7 +115,7 @@ export default function WritePostPage() {
 
   return (
     <div className="container mx-auto px-52 flex flex-col pt-20 pb-8 font-serif">
-      <form onSubmit={handleSubmit(postHandler)}>
+      <form onSubmit={handleSubmit(publishHandler)}>
         <textarea
           name="title"
           id="title"
@@ -84,9 +124,10 @@ export default function WritePostPage() {
           className="resize-none overflow-hidden w-full border-0 text-5xl focus:outline-none"
           {...register('title', { required: true })}
         />
+        { errors.title && errors.title.type === 'required' && (<span className="auth-error">Please add title.</span>) }
         {
           selectedImage && (
-            <img src={URL.createObjectURL(selectedImage)} alt="Cover" className="mt-4" />
+            <img src={URL.createObjectURL(selectedImage)} alt="Cover" className="mt-4 mx-auto" />
           )
         }
         <div className="my-4 flex flex-row">
@@ -109,6 +150,7 @@ export default function WritePostPage() {
             selectedImage && (<button type="button" onClick={removeImage} className="ml-4 text-red-700">Remove</button>)
           }
         </div>
+        { errors.imageCover && errors.imageCover.type === 'required' && (<span className="auth-error">Please add cover image.</span>) }
         <Editor
           editorState={editorState}
           onEditorStateChange={handleEditorChange}
@@ -118,9 +160,17 @@ export default function WritePostPage() {
           toolbar={Toolbar}
         />
         {/* <div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)} /> */}
-        <div className="flex flex-row-reverse items-center mt-4">
-          <input type="submit" value="Publish" className="cursor-pointer py-2 px-6 bg-black text-gray-100 rounded-lg transform transition duration-300 hover:text-white hover:bg-gray-900" />
-          <button type="button" className="mr-4 text-gray-700" onClick={goBack}>Cancel</button>
+        <div className="flex flex-row-reverse items-center my-4">
+          {
+            isPublishing ? (
+              <p className="pt-4">Publishing on progress...</p>
+            ) : (
+              <>
+                <input type="submit" value="Publish" className="cursor-pointer py-2 px-6 bg-black text-gray-100 rounded-lg transform transition duration-300 hover:text-white hover:bg-gray-900" />
+                <button type="button" className="mr-4 text-gray-700" onClick={goBack}>Cancel</button>
+              </>
+            )
+          }
         </div>
       </form>
     </div>
