80cf5b1db2bfd44518af548fd5f63d63262e9ba3
racmathafidz <rac.hafidz@gmail.com>
2021-11-17 07:33:33 +0700

    feat(email notification): add email notification feature when new article post

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,17 @@
+import emailjs from 'emailjs-com';
+
+export default function sendEmail(articleUrl) {
+  const templateParams = {
+    to_name: 'Wrytte Admin',
+    message: `http://localhost:3000${articleUrl}`,
+  };
+
+  emailjs.send(
+    process.env.REACT_APP_EMAIL_SERVICE_ID,
+    process.env.REACT_APP_EMAIL_TEMPLATE_ID,
+    templateParams,
+    process.env.REACT_APP_EMAIL_USER_ID,
+  )
+    .then(() => console.log('Email sent succesfully.'))
+    .catch((err) => console.log(err));
+}
