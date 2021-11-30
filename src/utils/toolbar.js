db56036c97464e63aa2850820ddf7fec936fdefc
racmathafidz <rac.hafidz@gmail.com>
2021-12-01 05:03:32 +0700

    refactor(toolbar): refactoring editor's toolbar

--- src/utils/toolbar.js
+++ src/utils/toolbar.js
@@ -1,8 +1,15 @@
 const toolbar = {
   options: ['inline', 'blockType', 'list', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
+  inline: {
+    inDropdown: false,
+    className: undefined,
+    component: undefined,
+    dropdownClassName: undefined,
+    options: ['bold', 'italic', 'underline', 'monospace'],
+  },
   blockType: {
     inDropdown: true,
-    options: ['Normal', 'H1', 'H2', 'H3', 'Blockquote', 'Code'],
+    options: ['Normal', 'H1', 'H2', 'H3', 'Code'],
     className: undefined,
     component: undefined,
     dropdownClassName: undefined,
